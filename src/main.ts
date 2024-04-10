3210e96121ac69b6da7331e16ab5ef2cf7a36b51
Javier Sanchez Collado <colladonhijodecolladan@gmail.com>
2024-04-10 18:38:38 +0200

    h

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,10 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
+    
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(withInterceptors([baseUrlInterceptor, authTokenInterceptor])),
   ],
-});
+})
